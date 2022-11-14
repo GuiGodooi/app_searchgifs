2de23debe2f3e6a764b4146bd0c92be0f3f46c80
GuiGodooi <gui.go.d@hotmail.com>
2022-11-14 14:24:28 -0300

    Second screen

--- lib/model/repository/gif_repository.dart
+++ lib/model/repository/gif_repository.dart
@@ -7,6 +7,7 @@ class GifReposotiry {
     Uri url = Uri.parse(
       'https://api.giphy.com/v1/gifs/trending?api_key=oGye8QvdJA8XjGTkIm96RODnzZzFhgrk&limit=25&rating=g',
     );
+
     List<Gif> gifList = [];
 
     final resultado = await http.get(url);
@@ -20,4 +21,23 @@ class GifReposotiry {
     }
     return gifList;
   }
+
+  // Future<List<Gif>> searchGif() async {   API SEARCH
+  //   Uri url = Uri.parse(
+  //     'https://api.giphy.com/v1/gifs/search?api_key=jhqvxq3weCvnyk5JRBM3HOPLJrF2P6Yp&q=dogs&limit=25&offset=0&rating=g&lang=en',
+  //   );
+  //   List<Gif> searchList = [];
+
+  //   final resultado = await http.get(url);
+  //   if (resultado.statusCode == 200) {
+  //     Map json = const JsonDecoder().convert(resultado.body);
+
+  //     for (var element in (json['data'] as List)) {
+  //       searchList.add(Gif.fromJson(element));
+  //     }
+
+  //     return searchList;
+  //   }
+  //   return searchList;
+  // }
 }
